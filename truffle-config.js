ad5052a6842535f8801aaa31910b481bb8313232
Karega McCoy <karega.mccoy@gmail.com>
2023-03-29 07:57:10 -0500

    Updates

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,9 @@
+require("dotenv").config();
+
+const { ETHERSCAN_API, GOERLI_ETHERSCAN_API, MNEMONIC, PROJECT_ID } = process.env;
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
     /**
      * Networks define how you connect to your ethereum client and let you set the
@@ -10,37 +16,14 @@ module.exports = {
      */
 
     networks: {
-        // Useful for testing. The `development` name is special - truffle uses it by default
-        // if it's defined here and no other network is specified at the command line.
-        // You should run a client (like ganache, geth, or parity) in a separate terminal
-        // tab if you use this network and you must also set the `host`, `port` and `network_id`
-        // options below to some value.
-        //
-        // development: {
-        //  host: "127.0.0.1",     // Localhost (default: none)
-        //  port: 8545,            // Standard Ethereum port (default: none)
-        //  network_id: "*",       // Any network (default: none)
-        // },
-        //
-        // An additional network, but with some advanced options…
-        // advanced: {
-        //   port: 8777,             // Custom port
-        //   network_id: 1342,       // Custom network
-        //   gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-        //   gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-        //   from: <address>,        // Account to send transactions from (default: accounts[0])
-        //   websocket: true         // Enable EventEmitter interface for web3 (default: false)
-        // },
-        //
-        // Useful for deploying to a public network.
-        // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-        //
-        // Useful for private networks
-        // private: {
-        //   provider: () => new HDWalletProvider(MNEMONIC, `https://network.io`),
-        //   network_id: 2111,   // This network is yours, in the cloud.
-        //   production: true    // Treats this network as if it was a public net. (default: false)
-        // }
+        goerli: {
+            provider: () => new HDWalletProvider(
+                MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`
+            ),
+            network_id: 5,
+            gasPrice: 10e9,
+            skipDryRun: true
+        }
     },
 
     /**
@@ -57,18 +40,16 @@ module.exports = {
 
     compilers: {
         solc: {
-            version: "0.7.6", // Fetch exact version from solc-bin (default: truffle's version)
-            // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+            version: "0.7.6",       // Fetch exact version from solc-bin (default: truffle's version)
+            docker: false,          // Use "0.5.1" you've installed locally with docker (default: false)
             settings: {
                 // See the solidity docs for advice about optimization and evmVersion
                 optimizer: {
                     enabled: true,
                     runs: 1
-                },
-                //  evmVersion: "byzantium"
-                // }
-            },
-        },
+                }
+            }
+        }
         // Truffle DB is currently disabled by default; to enable it, change enabled:
         // false to enabled: true. The default storage location can also be
         // overridden by specifying the adapter settings, as shown in the commented code below.
@@ -89,4 +70,32 @@ module.exports = {
         //     }
         //   }
     },
+
+    /**
+     * Configure your plugins
+     */
+
+    plugins: [
+        "truffle-plugin-verify"
+    ],
+
+    /**
+     * Configure your api keys
+     */
+
+    api_keys: {
+        etherscan: ETHERSCAN_API,
+        goerli_etherscan: GOERLI_ETHERSCAN_API
+    },
+
+    /**
+     * Configure HTTP proxy
+     */
+
+    verify: {
+        proxy: {
+            host: '127.0.0.1',
+            port: '24012'
+        }
+    }
 };
